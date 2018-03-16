d017f0786861c374edfbbabad8902db85552d82b
ssaegert <ssaegert@gmail.com>
2018-03-16 12:08:05 -0500

    Changed some parameters

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,11 +2,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist       VARCHAR(100) NOT NULL,
-    name         VARCHAR(100) NOT NULL,
+    artist       VARCHAR(150) NOT NULL,
+    name         VARCHAR(150) NOT NULL,
     release_date INT UNSIGNED NOT NULL,
-    sales        FLOAT NOT NULL,
-    genre        VARCHAR (255) NOT NULL,
+    sales        DECIMAL(6, 2) NOT NULL,
+    genre        VARCHAR (150) NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE contacts (
