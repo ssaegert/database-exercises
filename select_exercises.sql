8cd98c421e1d7bcd2c29c5573254587aff72f5fb
ssaegert <ssaegert@gmail.com>
2018-03-19 11:04:19 -0500

    Completed exercise 2 of Select

---
+++ select_exercises.sql
@@ -0,0 +1,24 @@
+USE codeup_test_db;
+SELECT 'All Pink Floyd Albums' AS 'Exercise 2A';
+SELECT name FROM albums
+WHERE artist = 'pink floyd';
+
+SELECT 'Sgt. Peppers Lonely Hearts Club Band release date' AS 'Exercise 2B';
+SELECT release_date FROM albums
+WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT 'Genre for Nevermind' AS 'Exercise 2C';
+SELECT genre FROM albums
+WHERE name = 'Nevermind';
+
+SELECT 'Albums from the 90s' AS 'Exercise 2D';
+SELECT name FROM albums
+WHERE release_date > 1990 && release_date < 2000;
+
+SELECT 'Albums with sales lower than $20M' AS 'Exercise 2E';
+SELECT name FROM albums
+WHERE sales < 20.00;
+
+SELECT 'All rock Albums' AS 'Exercise 2F';
+SELECT name FROM albums
+WHERE genre = 'Rock';
