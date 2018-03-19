08ff7ec97bae7352eefdc0a261b8c0398dd3f2d6
ssaegert <ssaegert@gmail.com>
2018-03-19 11:20:31 -0500

    Complete exercise 3 on Select

--- select_exercises.sql
+++ select_exercises.sql
@@ -12,13 +12,14 @@ SELECT genre FROM albums
 WHERE name = 'Nevermind';
 
 SELECT 'Albums from the 90s' AS 'Exercise 2D';
-SELECT name FROM albums
-WHERE release_date > 1990 && release_date < 2000;
+SELECT * FROM albums
+-- WHERE release_date > 1990 && release_date < 2000;
+WHERE BETWEEN 1990 AND 1999;
 
 SELECT 'Albums with sales lower than $20M' AS 'Exercise 2E';
-SELECT name FROM albums
+SELECT * FROM albums
 WHERE sales < 20.00;
 
 SELECT 'All rock Albums' AS 'Exercise 2F';
-SELECT name FROM albums
+SELECT * FROM albums
 WHERE genre = 'Rock';
---
+++ update_exercises.sql
@@ -0,0 +1,25 @@
+USE codeup_test_db;
+
+SELECT name FROM albums;
+
+
+UPDATE albums
+SET sales = sales * 10
+WHERE id = 4;
+
+SELECT name FROM albums
+WHERE release_date < 1980;
+
+
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+
+
+
+SELECT * FROM albums
+WHERE artist = 'Michael Jackson';
+
+UPDATE albums
+SET name = 'Peter Jackson'
+WHERE name = 'Michael Jackson';
