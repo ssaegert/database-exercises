65a050bf67f0eb204c271b6b1a709af1f4206b17
ssaegert <ssaegert@gmail.com>
2018-03-20 16:10:44 -0500

    updates MySQL Join exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,4 +1,4 @@
-USE join_test_db;
+USE employees;
 
 SELECT CONCAT(
            emp_no, ' - ', last_name, ', ', first_name
@@ -13,32 +13,4 @@ SELECT first_name, last_name, dept_no
 FROM employees AS A
 JOIN dept_emp emp ON A.emp_no = emp.emp_no;
 
-CREATE DATABASE join_test_db;
 
-CREATE TABLE roles (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE users (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  role_id INT UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES roles (id)
-);
-
-INSERT INTO roles (name) VALUES ('admin');
-INSERT INTO roles (name) VALUES ('author');
-INSERT INTO roles (name) VALUES ('reviewer');
-INSERT INTO roles (name) VALUES ('commenter');
-
-INSERT INTO users (name, email, role_id) VALUES
-  ('bob', 'bob@example.com', 1),
-  ('joe', 'joe@example.com', 2),
-  ('sally', 'sally@example.com', 3),
-  ('adam', 'adam@example.com', 3),
-  ('jane', 'jane@example.com', null),
-  ('mike', 'mike@example.com', null);
---
+++ join_test_db.sql
@@ -0,0 +1,37 @@
+USE join_test_db;
+
+CREATE DATABASE join_test_db;
+
+CREATE TABLE roles (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  role_id INT UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id) VALUES
+  ('bob', 'bob@example.com', 1),
+  ('joe', 'joe@example.com', 2),
+  ('sally', 'sally@example.com', 3),
+  ('adam', 'adam@example.com', 3),
+  ('jane', 'jane@example.com', null),
+  ('mike', 'mike@example.com', null);
+
+INSERT INTO users (name, email, role_id) VALUES
+  ('scott', 'scott@example.com', 2),
+  ('nathan', 'nathan@example.com', 2),
+  ('jim', 'jim@example.com', 2),
+  ('john', 'john@example.com', null);
