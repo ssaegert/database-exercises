510d5af7d0df98ac9d5c6cdbac4a82d3e91bc9a1
ssaegert <ssaegert@gmail.com>
2018-03-21 16:25:47 -0500

    Completed MySQL Join Bonus exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -24,18 +24,6 @@ ORDER BY `Department Name`;
 
 
 
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
-FROM employees as e
-  JOIN dept_manager as de
-    ON de.emp_no = e.emp_no
-  JOIN departments as d
-    ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' && e.gender = 'F'
-ORDER BY `Department Name`;
-
-
-
-
 SELECT t.title AS Title, COUNT(e.first_name) AS Count
 FROM employees as e
   JOIN titles as t
@@ -44,7 +32,7 @@ FROM employees as e
     ON de.emp_no = e.emp_no
   JOIN departments AS d
     ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' && d.dept_name = 'Customer Service' && t.to_date ='9999-01-01'
+WHERE d.dept_name = 'Customer Service' && t.to_date ='9999-01-01'
 GROUP BY Title;
 
 
@@ -57,14 +45,22 @@ FROM employees AS e
     ON d.dept_no = de.dept_no
   JOIN salaries AS s
     ON e.emp_no = s.emp_no
-WHERE de.to_date = '9999-01-01' && s.to_date = '9999-01-01'
+WHERE de.to_date > curdate() && s.to_date > curdate()
 ORDER BY `Department Name`;
 
+
+
+
+
+
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
-  first_name AS 'Manager Name'
+  d.dept_name AS 'Department Name',
+  CONCAT(e2.first_name, e2.last_name) AS 'Manager Name'
   FROM employees AS e
-    JOIN dept_manager AS de
-      ON de.emp_no = e.emp_no
-    JOIN departments AS d
-      ON d.dept_no = de.dept_no;
+    JOIN dept_emp AS de ON e.emp_no = de.emp_no
+    JOIN departments AS d ON de.dept_no = d.dept_no
+    JOIN dept_manager AS m ON d.dept_no = m.dept_no
+    JOIN employees AS e2 ON m.emp_no = e2.emp_no
+WHERE de.to_date > curdate() AND m.to_date > curdate()
+ORDER BY `Employee Name`;
 
