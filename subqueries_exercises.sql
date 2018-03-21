1a96697e7914839407540ebfa2b44402da301ac7
ssaegert <ssaegert@gmail.com>
2018-03-21 15:01:45 -0500

    Completed MySQL Sub Queries Bonus

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -19,13 +19,17 @@ WHERE e.first_name IN (
 );
 
 
-SELECT e.first_name, ' ', e.last_name
+SELECT e.first_name, e.last_name
 FROM employees as e
   JOIN dept_manager as de
     ON de.emp_no = e.emp_no
   JOIN departments as d
     ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' && e.gender = 'F';
+WHERE de.to_date IN (
+    SELECT to_date
+    FROM dept_manager
+    WHERE de.to_date = '9999-01-01'
+          && e.gender = 'F');
 
 
 SELECT d.dept_name
@@ -34,4 +38,32 @@ FROM employees as e
     ON de.emp_no = e.emp_no
   JOIN departments as d
     ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' && e.gender = 'F';
+WHERE de.to_date IN (
+  SELECT to_date
+  FROM dept_manager
+  WHERE de.to_date = '9999-01-01'
+        && e.gender = 'F')
+ORDER BY dept_name;
+
+
+
+SELECT e.first_name, e.last_name
+FROM employees AS e
+ JOIN salaries AS s
+    ON e.emp_no = s.emp_no
+WHERE s.salary IN (
+  SELECT salary
+  FROM salaries
+  WHERE salaries.to_date = '9999-01-01'
+)
+ORDER BY salary DESC
+LIMIT 1;
+
+
+
+SELECT e.first_name, e.last_name
+FROM employees AS e
+  JOIN salaries AS s
+    ON e.emp_no = s.emp_no
+ORDER BY salary DESC
+LIMIT 1;
